0025a6e597ed61feb06b69742d2c560230fa03fa
tymekk191 <tymoteuszmc@gmail.com>
2024-05-25 12:21:06 +0200

    Wstępny zarys projektu

--- Odczyt.py
+++ Odczyt.py
@@ -2,7 +2,7 @@ from Aforyzm import Aforyzm
 
 def wczytaj_aforzymy_tekst():
     aforyzmy = []
-    with open("aforyzmy.txt", "r", encoding="utf8") as plik:
+    with open("aforyzmy_testowe.txt", "r", encoding="utf8") as plik:
         for line in plik:
             line = line.strip() #usuniecie bialych znakow na koncu i poczatku danej linii
             if line.endswith("*"): #uwzgledniamy znak konczacy *
