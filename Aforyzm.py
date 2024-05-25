12e671689d967393f43301ddf08d82972bf872fa
tymekk191 <tymoteuszmc@gmail.com>
2024-05-25 14:14:40 +0200

    Odczyt aforzymów z mechanizmem ocen

--- Aforyzm.py
+++ Aforyzm.py
@@ -4,15 +4,43 @@ class Aforyzm:
         self.oceny = []
 
     def dodaj_ocene(self):
-        ocena = int(input("Podaj ocenę z zakresu od 0 do 5: "))
-        if ocena >= 0 and ocena <=5 and isinstance(ocena, int):
-            self.oceny.append(ocena)
-            print("Poprawnie dodano ocenę dla aforyzmu!")
-        else:
-            print("Błąd! Ocena spoza dozwolonego zakresu, bądź niecałkowita!")
+        while True:
+            try: #mechanizm dodawania oceny z obsługą błędu
+                ocena = int(input("Podaj ocenę z zakresu od 0 do 5: "))
+                if ocena >= 0 and ocena <=5:
+                    self.oceny.append(ocena)
+                    print("Poprawnie dodano ocenę dla aforyzmu!")
+                    break
+                else:
+                    print("Błąd! Ocena spoza dozwolonego zakresu, bądź niecałkowita!")
+            except ValueError:
+                print("Podano wartość niecałkowitą lub niedozwoloną dla oceny - wybierz jeszcze raz!")
 
+    def wyswietl_oceny(self): #wyświetlanie średniej ocen
+        print("\nŚrednia ocen dla tego aforyzmu to: ")
+        if len(self.oceny) == 0:
+            print("Brak ocen dla danego aforyzmu!")
+        else:
+            print(sum(self.oceny)/len(self.oceny))
+            print("Na podstawie danej ilości ocen: ", len(self.oceny))
+
+    #Główny mechanizm wyświetlania aforyzmów do main - funkcje powyżej
+    #to funkcje pomocniczne
     def wyswietl_aforyzm(self):
-        print("Aforyzm na dzisiejszy dzień to: ")
+        print("\nAforyzm na dzisiejszy dzień to: ")
         print(self.tekst)
+        self.wyswietl_oceny()
         print("\nCzy chcesz dodać ocenę dla powyższego aforzymu?")
-        print("Wybierz 1, jeśli tak lub 0, jeśli nie")
+        while True:
+            try: #mechanizm wyboru dodawania oceny z osbluga bledu (zabezpieczenie przed literami i niecalkowitymi)
+                print("Wybierz 1, jeśli tak lub 0, jeśli nie: ")
+                wybor_ocena = int(input())
+                if wybor_ocena == 1:
+                    self.dodaj_ocene()
+                    break
+                elif wybor_ocena == 0:
+                    break
+                else:
+                    print("Wybrano nieprawidłową opcję, wybierz jeszcze raz!")
+            except ValueError:
+                print("Podano wartość niecałkowitą lub niedozwoloną - wybierz jeszcze raz!")
--- main.py
+++ main.py
@@ -3,15 +3,24 @@ from Aforyzm import Aforyzm
 
 def main():
     aforyzmy = utworz_liste_aforyzmow()
-    wybor = 2
-    while(wybor != -1):
-        print("Aby wyśiwetlić aforyzm wciśnij 1")
+    while(True):
+        print("\n-----------------AFORYZMY NA KAŻDY DZIEŃ-----------------")
+        print("Aby wyświetlić aforyzm na dzisiejszy dzień wciśnij 1")
         print("Aby wyjść z programu wciśnij 0")
-        wybor = int(input("Podaj opcję: "))
-
-        if wybor == 1:
-            x=1
-            x=2
+        try: #proste menu z obsługą błędu
+            wybor = int(input("Podaj opcję: "))
+            if wybor == 1:
+                #tutaj będzie trzeba wrzucić mechanizm losowania aforyzmów
+                #zależy od pomysłu, można np. utworzyć dwie listy aforyzmów (uzyte/nieuzyte)
+                #i przezucac miedzy nimi - sprawdzcie sobie dzialanie teog na malym zbiorze
+                aforyzmy[3].wyswietl_aforyzm() # <- aforzym musi być wybierany po wartości na liście
+            elif wybor == 0:
+                print("Wybrano wyjście z programu!")
+                break
+            else:
+                print("Wybrano nieprawidłową, niedostępną opcję - wybierz jeszcze raz!")
+        except ValueError:
+            print("Podano wartość niecałkowitą - wybierz jeszcze raz!")
 
 
 
