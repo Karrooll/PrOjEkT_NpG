eeb1c59425f7e4d51d2cff8943c4973c5b342f1c
Adam Lebioda <adamlebioda288@gmail.com>
2024-06-04 11:41:54 +0200

    dodano komentarze

--- czas.py
+++ czas.py
@@ -1,7 +1,7 @@
 import datetime
 
 def wybierz_aforyzm_na_dzien(aforyzmy, data):
-    # Oblicz różnicę dni między podaną datą a datą 2024-06-05
+    # Oblicz różnicę dni między podaną datą a datą 2024-06-5
     indeks = (data - datetime.date(2024, 6, 5)).days % len(aforyzmy)
     # Wybierz aforyzm na podstawie obliczonego indeksu
     return aforyzmy[indeks]
@@ -16,9 +16,9 @@ def przesun_date_programu(obecna_data):
         nowa_data = obecna_data + datetime.timedelta(days=liczba_dni)
         # Wyświetl nową datę
         print("Nowa data: ", nowa_data)
+        # Zwróć nową datę
         return nowa_data
     except ValueError:
         # Obsłuż błędne dane wejściowe
         print("Podano niepoprawną wartość, data nie została zmieniona.")
         return obecna_data
-
